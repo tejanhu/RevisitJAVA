b2f44e7c3a7b3694919328f3ed72222843d5580d
Hussein <tejanahmedhu@gmail.com>
2017-01-07 21:08:40 +0000

    commit problem solved with recursion commented

--- JavaRevision/src/problemsolvem/ProblemB.java
+++ JavaRevision/src/problemsolvem/ProblemB.java
@@ -7,39 +7,67 @@ package problemsolvem;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * Exercise Task: Find the differences between two arrays of a similar size
  */
 public class ProblemB {
 
-    public static int getAbsDifferences(ArrayList<Integer> l1, ArrayList<Integer> l2) {
+    public static int getDifference(ArrayList<Integer> l1, ArrayList<Integer> l2, int n) {
         int diff = 0;
+        int m = 0;
+        Scanner input = new Scanner(System.in);
+
+        /**
+         * fill up array lists with some elements
+         *
+         */
+        for (int i = 0; i < n; i++) {
+            System.out.println("List1:");
+            System.out.println("Please enter an integer: ");
+            m = input.nextInt();
+            l1.add(m);
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("\nList2:");
+            System.out.println("Please enter an integer: ");
+            m = input.nextInt();
+            l2.add(m);
+
+        }
+
+        /**
+         * calculate absolute difference between both arrays
+         *
+         */
         for (int i = 0; i < l1.size(); i++) {
             diff += Math.abs(l1.get(i) - l2.get(i));
         }
         return diff;
     }
 
-    public static void main(String[] args) {
+//    public static int getTotalDiff(ArrayList<Integer> l1, ArrayList<Integer> l2,int n) {
+//        int size = l1.size();
+//        if (size == 0) {
+//            return 0;
+//        }
+//        int lastElementDiff=Math.abs(l1.get(size-1)-l2.get(size-1));
+//        int diff=getTotalDiff(l1, l2, size-1)+lastElementDiff;
+//        return diff;
+//    }
+
+
+public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<Integer>();
         ArrayList<Integer> list2 = new ArrayList<Integer>();
 
-        list.add(2);
-        list.add(4);
-        list.add(50);
-        list.add(3);
-        list.add(6);
-        list.add(9);
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter a single integer: ");
+        int n = input.nextInt();
 
-        list2.add(3);
-        list2.add(50);
-        list2.add(7);
-        list2.add(13);
-        list2.add(16);
-        list2.add(10);
-
-        System.out.println("Total difference: " + getAbsDifferences(list, list2));
-
+        System.out.println("");
+        System.out.println("\nTotal difference: " + getDifference(list, list2, n));
     }
 }
